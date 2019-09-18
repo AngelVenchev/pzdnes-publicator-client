b3b5a7bd40e76fea2e3e0a557a6fa0cfea19a418
Angel Venchev <angelvenchev@yahoo.com>
2019-09-18 10:58:00 +0300

    Updates

---
+++ precache-manifest.e39f039b9c745266755a1dffc5385a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111f8d340d60a3f3ccfe16de3edd51fd",
+    "url": "/pzdnes-publicator-client/index.html"
+  },
+  {
+    "revision": "dcbb8d31ef69471c7b77",
+    "url": "/pzdnes-publicator-client/static/css/main.7503944c.chunk.css"
+  },
+  {
+    "revision": "d19abec4300bd3901e68",
+    "url": "/pzdnes-publicator-client/static/js/2.0efc6842.chunk.js"
+  },
+  {
+    "revision": "dcbb8d31ef69471c7b77",
+    "url": "/pzdnes-publicator-client/static/js/main.197d5b3b.chunk.js"
+  },
+  {
+    "revision": "e80d9ccb799aadc1fa94",
+    "url": "/pzdnes-publicator-client/static/js/runtime~main.ac81d7f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pzdnes-publicator-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
